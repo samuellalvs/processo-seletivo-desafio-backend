36f11ad5b3a36cbed6c6862533c4d2c0e642b3f9
Samuel Alves <samuellmsl05@gmail.com>
2022-01-14 18:40:27 -0300

    :art: Atualização na tela home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,7 +12,7 @@
 
 <div class="container">
     <div class="header">
-        <h1>Olá, Samuel</h1>
+        <h1>Olá, <b>Samuel</b></h1>
 
         <button>Sair</button>
     </div>
@@ -20,13 +20,14 @@
     <div class="content">
         <div class="bank-balance-wrapper">
             <p>Seu saldo é de</p>
-            <h3>R$10.000,00</h3>
+            <h2>R$10.000,00</h2>
         </div>
 
-        <div class="actions-wrapper">
-            <button>
-                Fazer transferencia
-            </button>
+        <h2 class="title">Serviços disponiveis</h2>
+        <div class="services-wrapper">
+            <a href={{route('transfer')}}>
+                Fazer transferência
+            </a>
         </div>
     </div>
 </div>
