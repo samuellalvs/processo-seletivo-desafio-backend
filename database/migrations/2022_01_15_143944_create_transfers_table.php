b9a5b6f5020f454606ea5c853a04f18d291e620f
Samuel Alves <samuellmsl05@gmail.com>
2022-01-15 13:41:47 -0300

    :card_file_box: Migrations das tabelas

---
+++ database/migrations/2022_01_15_143944_create_transfers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_sender_id');
+            $table->foreign('user_sender_id')->references('id')->on('user');
+            $table->unsignedBigInteger('user_beneficiary_id');
+            $table->foreign('user_beneficiary_id')->references('id')->on('user');
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+}
